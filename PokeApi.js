9acb9ba3cdc488d3772ced4303225b803d331ba5
Camilo Guerrero <guerrerocontrerascristiancamil@gmail.com>
2023-03-30 19:01:19 -0500

    Agregando estilos

--- PokeApi.js
+++ PokeApi.js
@@ -20,10 +20,26 @@ const getAll = async () => {
 				let res = await fetch(json.results[i].url),
 					pokemon = await res.json();
 
+				console.log(pokemon);
 				if (!res.ok) throw { status: res.status, statusText: err.statusText };
 
 				template += `
-          <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+        <figure class="poke-card">
+			    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+			    <h2>${pokemon.name}</h2>
+          <h3>Altura</h3>
+			    <p>${pokemon.height}</p>
+			    
+          <h3>Habilidades</h3>
+          <p>${pokemon.abilities[0].ability.name}</p>
+			    
+				  <h3>Movimientos</h3>
+				  <p>${pokemon.moves[0].move.name}</p>
+				  <p>${pokemon.moves[1].move.name}</p>
+				  <p>${pokemon.moves[2].move.name}</p>
+			    
+		    </figure>  
+
         `;
 			}
 		} catch (err) {
---
+++ README.md
@@ -0,0 +1,8 @@
+# Hola! soy camilo guerrero
+
+## un estudiante de programación que quiere practicas con proyectos mientras aumenta el conocimiento
+
+podemos practicar juntos, mis redes sociales son:
+
+- [Facebook](https://www.facebook.com/camilo.guerrero.3597789/)
+- [Twitter](https://twitter.com/cristian321893)
